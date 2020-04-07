1395dc92365542c53d3ea5f864bb082b7c7fa4d0
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-04-07 16:12:58 +0545

    before passing data between screens

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import Navigator from "./routes/homeStack";
+
+import Home from "./screens/home";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-light": require("./assets/fonts/Nunito-Light.ttf"),
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-semibold": require("./assets/fonts/Nunito-SemiBold.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return <Navigator />;
+  } else {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
+  }
+}
---
+++ screens/home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { globalStyles } from "../styles/global";
+
+export default function Home({ navigation }) {
+  const pressHandler = () => {
+    navigation.navigate("ReviewDetails");
+    // navigation.push("ReviewDetails");
+  };
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>
+        This is Home Screen ... Please check if it is not.
+      </Text>
+      <Button title="go to review" onPress={pressHandler} />
+    </View>
+  );
+}
