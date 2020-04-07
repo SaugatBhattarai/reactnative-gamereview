d724f81ec9654ab394b51cd9d7aeaf165c47d2fa
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-04-07 16:44:07 +0545

    passing value through screen

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -6,12 +6,32 @@ import ReviewDetails from "../screens/reviewDetails";
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: "Home: Movie Rating",
+      //   headerStyle: {
+      //     backgroundColor: "#eee",
+      //   },
+    },
   },
   ReviewDetails: {
     screen: ReviewDetails,
+    navigationOptions: {
+      title: "Review Details: Movie Rating",
+      //   headerStyle: {
+      //     backgroundColor: "#eee",
+      //   },
+    },
   },
 };
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: "#444",
+    headerStyle: {
+      backgroundColor: "#ddd",
+      height: 60,
+    },
+  },
+});
 
 export default createAppContainer(HomeStack);
